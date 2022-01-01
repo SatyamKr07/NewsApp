4b1429eaf7d9b79f5b6a7e25b6cd9e32e2d6c2f3
Satyam <satyamismyname@gmail.com>
2022-01-01 23:25:16 +0530

    news_model

---
+++ lib/src/central/strings.dart
@@ -0,0 +1,1 @@
+String newsApiKey = "5d6a9967f4cc40d3991cf59116e5c149";
--- lib/src/controllers/home_news_controller.dart
+++ lib/src/controllers/home_news_controller.dart
@@ -1,3 +1,24 @@
+import 'package:dio/dio.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:news_app/src/central/my_logger.dart';
 
-class HomeNewsController extends GetxController {}
+class HomeNewsController extends GetxController {
+  Dio dio = Dio();
+  Future getNews() async {
+    String apiUri =
+        "https://newsapi.org/v2/everything?q=bitcoin&apiKey=5d6a9967f4cc40d3991cf59116e5c149";
+    try {
+      Response response = await dio.get(apiUri);
+      logger.d('getNews response $response');
+    } catch (e) {
+      logger.e('getNews error $e');
+    }
+  }
+
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+    getNews();
+  }
+}
